2180423c503fc330cd38f2166d7afa65173cbba4
Jay Gupta <jay002@e.ntu.edu.sg>
2021-04-10 17:55:04 +0800

    changed model-controller config

--- login-struts/src/main/java/org/apache/struts/loginstruts/action/LoginAction.java
+++ login-struts/src/main/java/org/apache/struts/loginstruts/action/LoginAction.java
@@ -1,63 +1,21 @@
 package org.apache.struts.loginstruts.action;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
 import org.apache.struts.loginstruts.model.LoginModel;
-
 import com.opensymphony.xwork2.ActionSupport;
-
-
 public class LoginAction extends ActionSupport {
-
    private static final long serialVersionUID = 1L;
-
    private String errorMessage;
-    
-    private LoginModel loginModel = new LoginModel();
+   private LoginModel loginModel = new LoginModel();
     
     public String execute() {
-    	
         String ret = ERROR;
-        Connection conn = null;
+        boolean validateUser = loginModel.validateUser();
 
-        try {
-           String URL = "jdbc:mysql://localhost/STRUTS_LOGIN";
-           
-           conn = DriverManager.getConnection(URL, "root", "password");
-           String sql = "SELECT userNAME FROM LOGIN WHERE";
-           
-           sql+=" userNAME = ? AND password = ?";
-           PreparedStatement ps = conn.prepareStatement(sql);
-           
-           ps.setString(1, loginModel.getUsername());
-           ps.setString(2, loginModel.getPassword());
-           
-           ResultSet rs = ps.executeQuery();
-
-           while (rs.next()) {
-              ret = SUCCESS;
-           }
-        } catch (Exception e) {
-        	System.out.println(e);
-           ret = ERROR;
-        } finally {
-           if (conn != null) {
-              try {
-                 conn.close();
-              } catch (Exception e) {
-            	  System.out.println(e);
-              }
-           }
-        }
-        
-        if(ret != SUCCESS)
+        if(!validateUser)
         	setErrorMessage("Unsuccessful Login, Please try again");
-        
-        return ret;
-        
+        else
+         ret = SUCCESS;
+
+        return ret;   
     }
     
     public LoginModel getLoginModel() {
--- login-struts/src/main/java/org/apache/struts/loginstruts/model/LoginModel.java
+++ login-struts/src/main/java/org/apache/struts/loginstruts/model/LoginModel.java
@@ -1,7 +1,9 @@
 package org.apache.struts.loginstruts.model;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 public class LoginModel {
-	
 	private String username;
 	private String password;
 	
@@ -29,4 +31,38 @@ public class LoginModel {
 		this.password = password;
 	}
 
+	public boolean validateUser() {
+		Connection conn = null;
+
+        try {
+           String URL = "jdbc:mysql://localhost/STRUTS_LOGIN";
+           
+           conn = DriverManager.getConnection(URL, "root", "password");
+           String sql = "SELECT userNAME FROM LOGIN WHERE";
+           
+           sql+=" userNAME = ? AND password = ?";
+           PreparedStatement ps = conn.prepareStatement(sql);
+           
+           ps.setString(1, this.getUsername());
+           ps.setString(2, this.getPassword());
+           
+           ResultSet rs = ps.executeQuery();
+
+           while (rs.next()) {
+              return true;
+           }
+        } catch (Exception e) {
+        	System.out.println(e);
+           	return false;
+        } finally {
+           if (conn != null) {
+              try {
+                 conn.close();
+              } catch (Exception e) {
+            	  System.out.println(e);
+              }
+           }
+        }
+		return false;
+	}
 }
